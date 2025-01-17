3740bd96b8f6bd7702e3698776db3b7a8cec4864
riiddhii757 <riddhhi757@gmail.com>
2025-01-17 12:31:00 -0500

    Add Avatar component

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./details";
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar img={props.img}/>
+      </div>
+      <div className="bottom">
+        <Detail detailInfo ={props.tel}/>
+        <Detail detailInfo ={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
